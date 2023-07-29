2d9b419139520d8c452e536fb8b37ad519a4d75c
Giulio Fantuzzi <74678041+giuliofantuzzi@users.noreply.github.com>
2023-07-29 12:32:09 +0200

    Update tableconverter.py

--- tableconverter.py
+++ tableconverter.py
@@ -1,26 +1,33 @@
-#-----------------------------------------------------------------------------
-# If not already installed
-# pip install pandas
-# pip install tabulate
-#-----------------------------------------------------------------------------
+#pip install pandas
+#pip install tabulate
 import pandas as pd
 from tabulate import tabulate
-
 def csv_to_latex(csv_file, output_file):
-    # Read CSV file using pandas
+    # Leggo il file CSV utilizzando pandas
     df = pd.read_csv(csv_file)
-    # Convert DataFrame to LaTeX table.
+    # Converto il DataFrame in una tabella LaTeX
     latex_table = tabulate(df, tablefmt="latex", headers="keys", showindex=False)
-    # Save table as a .tex output file
+    # Scrivo la tabella LaTeX nel file di output
     with open(output_file, "w") as f:
         f.write(latex_table)
 
-#-----------------------------------------------------------------------------
-# Replace "input.csv" with the path of the CSV to convert
-input_csv_file = "input.csv"
 
-# Replace "output.csv" with the path where to save the .tex file
-output_latex_file = "output.tex"
+# The user insert as input the csv file to convert
+input_csv_file = input('Insert the name of the csv file to convert:\n')
+# Output file: same filename but change the extension .csv to .tex 
+output_latex_file = input_csv_file[0:len(input_csv_file)-4]+".tex"
 
-csv_to_latex(input_csv_file, output_latex_file)
-#-----------------------------------------------------------------------------
+# Conversion
+try:
+    csv_to_latex(input_csv_file, output_latex_file)
+    conversionmessage= 'Conversion completed successfully!'
+    print('-'*len(conversionmessage))
+    print(conversionmessage)
+    print('-'*len(conversionmessage))
+except:
+    errormessage= 'Conversion failed!'
+    print('-'*len(errormessage))
+    print(errormessage)
+    print('-'*len(errormessage))
+
+
